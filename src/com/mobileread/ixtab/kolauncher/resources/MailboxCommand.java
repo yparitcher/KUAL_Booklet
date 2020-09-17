02463554004be4fda75dc42c83569aa83fec2a9e
yparitcher <y@paritcher.com>
2020-09-16 20:37:13 -0400

    delete unnecessary files

--- src/com/mobileread/ixtab/kolauncher/resources/MailboxCommand.java
+++
@@ -1,5 +0,0 @@
-package com.mobileread.ixtab.kolauncher.resources;
-
-public interface MailboxCommand {
-	public void execute(Object data);
-}
--- src/com/mobileread/ixtab/kolauncher/resources/ResourceLoader.java
+++
@@ -1,9 +0,0 @@
-package com.mobileread.ixtab.kolauncher.resources;
-
-import java.io.InputStream;
-
-public class ResourceLoader {
-	public static InputStream load(String name) {
-		return ResourceLoader.class.getResourceAsStream(name);
-	}
-}
